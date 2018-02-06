459a41aa4a1b84ffcfe6986f4e80ee50b47d7298
qwuxinhua@163.com <qwuxinhua@163.com>
2018-02-06 11:37:42 +0800

    Initial commit

---
+++ src/main/java/com/wxh/SpringCloudEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.wxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *  启动Eureka服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
